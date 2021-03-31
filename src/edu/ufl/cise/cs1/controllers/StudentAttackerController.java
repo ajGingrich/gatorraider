918c7cc8d18c2d7dce63a23d18430e9eaa667888
Andrew Gingrich <gandrew320@gmail.com>
2021-03-31 14:33:35 -0600

    add some basic methods and go to power pill

--- src/edu/ufl/cise/cs1/controllers/StudentAttackerController.java
+++ src/edu/ufl/cise/cs1/controllers/StudentAttackerController.java
@@ -4,6 +4,7 @@ import game.models.*;
 import java.awt.*;
 import java.util.List;
 
+/* This is the only place where code where be added */
 public final class StudentAttackerController implements AttackerController
 {
 	public void init(Game game) { }
@@ -12,20 +13,52 @@ public final class StudentAttackerController implements AttackerController
 
 	public int update(Game game,long timeDue)
 	{
-		int action;
+		// default action is empty
+		int action = Game.Direction.EMPTY;
+		// the actor to be controlled
+		Attacker gator = game.getAttacker();
 
-		//An example (which should not be in your final submission) of some syntax that randomly chooses a direction for the attacker to move
-		List<Integer> possibleDirs = game.getAttacker().getPossibleDirs(true);
-		if (possibleDirs.size() != 0)
-			action = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
-		else
-			action = -1;
+		// An example (which should not be in your final submission) of some syntax that randomly chooses a direction for the attacker to move
+		List<Integer> possibleDirs = gator.getPossibleDirs(true);
 
-		//An example (which should not be in your final submission) of some syntax to use the visual debugging method, addPathTo, to the top left power pill.
-		List<Node> powerPills = game.getPowerPillList();
-		if (powerPills.size() != 0) {
-			game.getAttacker().addPathTo(game, Color.BLUE, powerPills.get(0));
+		// the gator can't move anywhere
+		if (possibleDirs.size() == 0) {
+			return action;
 		}
+
+		// TODO: get a list of the all the power pills/defenders/pills
+		// figure out which is the closest with the
+		List<Node> powerPills = game.getPowerPillList();
+		List<Defender> defenders = game.getDefenders();
+		List<Node> regularPills = game.getPillList();
+
+		// get the closest power pills
+		if (powerPills.size() != 0) {
+			Node closestPowerPill = gator.getTargetNode(powerPills, true);
+			// use this to determine the direction for the defenders/pills
+			action = gator.getNextDir(closestPowerPill, true);
+			gator.addPathTo(game, Color.BLUE, closestPowerPill);
+		}
+
+		game.getDefenders();
+
+
+//		action = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+
+
+		// actor
+		// getLocation - returns current location of the actor
+
+		// game
+		// getDefenders
+
+		// defender interface
+		// isVulnerable
+
+		// with two nodes -> get path distance
+
+		// An example (which should not be in your final submission) of some syntax to use the visual debugging method, addPathTo, to the top left power pill.
+
 
 		return action;
 	}
